b9a38505ae6b9f35fccbff318970210177c83890
Luiz Alberto Alano <alano@luigipietro.com.br>
2021-03-09 22:35:52 -0300

    Commit 1 - Aula de layout linear

--- DIOIMCapplication/app/src/main/java/br/com/luigipietro/dio_imc_app/MainActivity.kt
+++ DIOIMCapplication/app/src/main/java/br/com/luigipietro/dio_imc_app/MainActivity.kt
@@ -2,10 +2,37 @@ package br.com.luigipietro.dio_imc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle", "metodo onStart -> A tela está visível")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifecycle", "metodo onResume -> A tela está interagível")
+    }
+
+    override fun onPause() {
+        Log.w("lifecycle", "metodo onPause -> A tela está pausível")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.w("lifecycle", "metodo onStop -> A tela está parada")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.w("lifecycle", "metodo onDestroy -> A tela está destruída")
+        super.onDestroy()
     }
 }
